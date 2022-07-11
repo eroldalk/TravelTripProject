e907d3f05c518b8262005955b1127aa0986a4317
Erol Dalkılıç <eroldalg@gmail.com>
2022-07-11 17:48:37 +0300

    DbContext

---
+++ TravelTripProject/Models/Siniflar/Context.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProject.Models.Siniflar
+{
+    public class Context
+    {
+    }
+}
