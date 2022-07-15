1a12955b6099ea25271064445da9c676401871e8
Erol Dalkılıç <eroldalg@gmail.com>
2022-07-15 18:20:57 +0300

    blog list

--- TravelTripProject/Controllers/BlogController.cs
+++ TravelTripProject/Controllers/BlogController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProject.Models.Siniflar;
 
 namespace TravelTripProject.Controllers
 {
     public class BlogController : Controller
     {
         // GET: Blog
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var bloglar = c.Blogs.ToList();
+            return View(bloglar);
         }
     }
 }
