b9b0fe62fb9fbc242369d8cfd54785dffa1aa620
Erol Dalkılıç <eroldalg@gmail.com>
2022-07-15 18:48:31 +0300

    blog detail

--- TravelTripProject/Controllers/BlogController.cs
+++ TravelTripProject/Controllers/BlogController.cs
@@ -16,5 +16,10 @@ namespace TravelTripProject.Controllers
             var bloglar = c.Blogs.ToList();
             return View(bloglar);
         }
+        public ActionResult BlogDetay(int id)
+        {
+            return View();
+        }
+
     }
 }
