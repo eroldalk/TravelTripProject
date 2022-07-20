734c4b067ca0beeb93c34753f734fe0625d59dc6
Erol Dalkılıç <eroldalg@gmail.com>
2022-07-20 20:29:08 +0300

    comment

--- TravelTripProject/Controllers/BlogController.cs
+++ TravelTripProject/Controllers/BlogController.cs
@@ -16,10 +16,13 @@ namespace TravelTripProject.Controllers
             var bloglar = c.Blogs.ToList();
             return View(bloglar);
         }
+        BlogYorum by = new BlogYorum();
+
         public ActionResult BlogDetay(int id)
         {
-            var blogbul = c.Blogs.Where(x => x.ID == id).ToList();
-            return View(blogbul);
+          //  var blogbul = c.Blogs.Where(x => x.ID == id).ToList();
+          by.Deger1 = c.Blogs.Where(x => x.ID == id).ToList();
+            return View(by);
         }
 
     }
