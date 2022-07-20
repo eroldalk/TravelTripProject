b001e3a8d5e458ec851180a4fb3b661fe55ab438
Erol Dalkılıç <eroldalg@gmail.com>
2022-07-20 23:23:24 +0300

    comment2

--- TravelTripProject/Controllers/BlogController.cs
+++ TravelTripProject/Controllers/BlogController.cs
@@ -11,17 +11,23 @@ namespace TravelTripProject.Controllers
     {
         // GET: Blog
         Context c = new Context();
+        BlogYorum by = new BlogYorum();
         public ActionResult Index()
         {
-            var bloglar = c.Blogs.ToList();
-            return View(bloglar);
+           // var bloglar = c.Blogs.ToList();
+           by.Deger1 = c.Blogs.ToList();
+           by.Deger3 = c.Blogs.Take(3).ToList();
+            by.Deger4 = c.Yorumlars.Take(3).ToList();
+            return View(by);
         }
-        BlogYorum by = new BlogYorum();
+        
 
         public ActionResult BlogDetay(int id)
         {
           //  var blogbul = c.Blogs.Where(x => x.ID == id).ToList();
-          by.Deger1 = c.Blogs.Where(x => x.ID == id).ToList();
+            by.Deger1 = c.Blogs.Where(x => x.ID == id).ToList();
+            by.Deger2 = c.Yorumlars.Where(x => x.Blogid == id).ToList();
+
             return View(by);
         }
 
--- TravelTripProject/Models/Siniflar/BlogYorum.cs
+++ TravelTripProject/Models/Siniflar/BlogYorum.cs
@@ -9,5 +9,7 @@ namespace TravelTripProject.Models.Siniflar
     {
         public IEnumerable<Blog> Deger1 { get; set; }
         public IEnumerable<Yorumlar> Deger2 { get; set; }
+        public IEnumerable<Blog> Deger3 { get; set; }
+        public IEnumerable<Yorumlar> Deger4 { get; set; }
     }
 }
